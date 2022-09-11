f9eb6646f9338981294bc630bd54aecc47a3d5a3
Brandon Spear <BowDownToTheEpicTurkey@gmail.com>
2022-09-11 15:31:51 -0700

    updates ui.

--- TESTS.md
+++ TESTS.md
@@ -1,4 +1,28 @@
-Describe: recieve data = It should recieve data from user;
+Describe: MrRobo
 Test:"recieve data from user."
-Code:
+Code: "1"
 Expect: User data recived 
+
+Test:"It should ask you to redo if number is less than one."
+Code: "0"
+Expect: "please enter a larger number!"
+-failed
+
+Test:"It should return error if nothing is submitted"
+Code: ""
+Expect: "please enter a larger number!"
+-failed
+
+Test: "It should return an array of numbers from 0 to the user's inputted number"
+Code: "5";
+Expected Output: [0, 1, 2, 3, 4, 5]
+
+Test: "It should return an array with the numbers converted to a string"
+Code: "5";
+Expected Output: ["0, 1, 2, 3, 4, 5"]
+
+Describe: replaceElements()
+Test: "It should return an array with the number 1 and 2 replaced with their    respecive strings, "Beep!" and "Boop!".
+Code: numberArray = beepBoop(2);
+beepBoop(numberArray);
+Expected Output: ["0", "Beep!", "Boop!"]
--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,83 @@
-//Buisness Logic
-
-function roboRoger(number) {
-  if (number > 100) {
-    document.getElementById("numberTooHigh")
-    return "Whoa there cowboy, that number is too high! Try a number that fits the parameters! (hint, its in the instructions)It will "
+// //Buisness Logic
+function beepBoop(number) {
+  
+  const parsedNumber = parseInt(number, 10);
+  let arrayValues = [];
+  
+  if ((!Number.isInteger(parsedNumber))|| (parsedNumber <= 0)) {
+    return {
+      error: true,
+      message:"A positive Integer was not entered"
+    };
+  } else {
+    for ( i = 0 ; i <= parsedNumber; i += 1) {
+      arrayValues.push(i);
+    }
   }
+  const numberString = arrayValues.toString().split(","); //Putting a note here just so I dont forget about this in our meeting. I have a question regarding (",").
+  const outArrayString =  replaceElements(numberString);
+  
+  return outArrayString;
+}
+
+function replaceElements(numberString) {
+
+  const subFor1s = "Beep!";
+  const subFor2s = "Boop!";
+  const subFor3s = "Won't you be my neighbor?";
+  const subFor69s = "Heh Noice!";
+  const outputString = numberString.map (function (element) {  
+    if (element.includes(3)) {
+      return subFor3s;
+
+    } else if (element.includes(2)) {
+        return subFor2s;
+
+    } else if (element.includes(69)) {
+        return subFor69s
+        //("#noiceGif").show(); Putting a note here just so I dont forget about this in our meeting. I have a question regarding image.
+      
+    } else if (element.includes(1)) {
+        return subFor1s;
+    
+    } else {      
+        return element;
+    }  
+  });
+
+return outputString;
+}
+
+// UI logic
+
+function handleInput(event) {
+
+  const inputString = document.getElementById("inputNumber").value;
+  const outputString = beepBoop(inputString);
+
+  document.getElementById("outputList").innerText = outputString;
+  event.preventDefault();
+}
+
+
+window.addEventListener("load", function() {
+  document.getElementById("userInput").addEventListener("submit", handleInput);
+  
+  let form = document.querySelector("form");
+  let submitBtn = document.getElementById("submitBtn");
+  let resetBtn = document.getElementById("resetBtn");
+
+  submitBtn.removeAttribute("hidden");
+  
+  form.addEventListener("submit", function() {
+    submitBtn.setAttribute("hidden","hidden");
+    resetBtn.removeAttribute("class");
+  });
+
+  resetBtn.addEventListener("click", function() {
+    submitBtn.removeAttribute("hidden");
+    document.getElementById("results").innerText = null;
+    resetBtn.setAttribute("class","hidden")
+  });
+
+});
