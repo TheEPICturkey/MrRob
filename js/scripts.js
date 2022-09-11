e6ca22d864e913a16039503e4b77c90537ceb04e
Brandon Spear <BowDownToTheEpicTurkey@gmail.com>
2022-09-11 09:01:49 -0700

    it will push all commits to github.

---
+++ js/scripts.js
@@ -0,0 +1,7 @@
+//Buisness Logic
+
+function roboRoger(number) {
+  if (number > 100) {
+    document.getElementById("numberTooHigh")
+    return "Whoa there cowboy, that number is too high! Try a number that fits the parameters! (hint, its in the instructions)It will "
+  }
